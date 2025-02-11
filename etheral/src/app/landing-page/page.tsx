df2708727f68337a343d82146b3b77851db02dfa
izzaafkarinank <izzaafkarina14@gmail.com>
2025-02-12 00:42:08 +0700

    refactor: change import package

--- etheral/src/app/landing-page/page.tsx
+++ etheral/src/app/landing-page/page.tsx
@@ -1,15 +1,13 @@
-import { LandingPage } from "@/features/landingPage"
+import { LandingPage } from "@/features";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Etheral | Landing Page",
-    description: "Discover the Future of Decentralized Art & Finance",
-  };
+  title: "Etheral | Landing Page",
+  description: "Discover the Future of Decentralized Art & Finance",
+};
 
 const Landing = () => {
-    return (
-        <LandingPage />
-    )
-}
+  return <LandingPage />;
+};
 
 export default Landing;
