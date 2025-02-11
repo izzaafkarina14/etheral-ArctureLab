c620e747b182dfdd430708e2f4cb838691a590d3
izzaafkarinank <izzaafkarina14@gmail.com>
2025-02-12 00:39:21 +0700

    refactor: add scroll into by id when onclick

--- etheral/src/components/navbar/Navbar.tsx
+++ etheral/src/components/navbar/Navbar.tsx
@@ -1,14 +1,40 @@
+"use client";
+
 import { Logo } from "../logo/Logo";
 
 export const Navbar = () => {
   return (
-    <div className="bg-transparent py-4">
+    <div className="fixed top-0 w-full z-50 bg-opacity-70 backdrop-blur-md py-4">
       <div className="flex items-center justify-between max-w-screen-xl mx-auto">
         <Logo />
-        <div className="flex text-primary gap-8">
-          <button>Home</button>
-          <button>Features</button>
-          <button>Explore</button>
+        <div className="flex text-primary gap-8 opacity-85">
+          <button
+            onClick={() =>
+              document
+                .getElementById("hero")
+                ?.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Home
+          </button>
+          <button
+            onClick={() =>
+              document
+                .getElementById("features")
+                ?.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Features
+          </button>
+          <button
+            onClick={() =>
+              document
+                .getElementById("explore")
+                ?.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Explore
+          </button>
           <button>Connect Wallet</button>
         </div>
       </div>
